452fb2ac723a2460dbcdf0bc5c1c65c2d6f4c157
Divider <89965004+it-cio@users.noreply.github.com>
2022-02-23 20:24:00 +0300

    Fix to_json test

--- tests/test_spare_parts.py
+++ tests/test_spare_parts.py
@@ -22,4 +22,4 @@ def test_add_part():
 
 def test_to_json():
     part = SparePart('Drum Unit for LaserJet 1020')
-    assert part.to_json() == '{"name": "Drum Unit for LaserJet 1020", "parts": []}'
+    assert part.to_json() == '{"name": "Drum Unit for LaserJet 1020", "parts": ["Drum Unit for LaserJet 1020"]}'
