7290f624e0ae10f7e0b92102e7e9f751faba6605
Divider <89965004+it-cio@users.noreply.github.com>
2022-03-05 21:21:52 +0300

    Create group for parts

--- classes/tmp.py
+++ classes/tmp.py
@@ -1,45 +1,45 @@
 from exceptions.ClassInitializationError import *
 
 
-class Report:
-    def __init__(self):
+class Equipment:
+    def __init__(self, name):
+        self.name = name
         self._dict = {}
 
-    def add_to(self, equipment):
-        self._dict.setdefault(equipment.group_name(), []).append(equipment)
+    def add_to(self, part):
+        self._dict.setdefault(self.name, []).append(part)
 
-    # def extract(self, name):
-    #     if self._dict[name]:
-    #         self._dict.setdefault(name).pop(0)
+    def extract(self, name):
+        if self._dict[name]:
+            self._dict.setdefault(name).pop(0)
+
     @property
     def dict(self):
         return self._dict
 
 
-class Equipment:
+class SparePart:
     def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.group = self.__class__.__name__
-
-    def group_name(self):
-        return f'{self.group}'
 
     def __repr__(self):
-        return f'{self.name}-{self.price}'
+        return f'{self.name} - {self.price}$'
 
 
-class SparePart(Equipment):
-    def __init__(self, name, price):
-        super().__init__(name, price)
+model_1 = Equipment('HP LaserJet 1020')
 
+part_1 = SparePart('Feed Drive for LaserJet 1020', 10)
+model_1.add_to(part_1)
+part_2 = SparePart('Repair kit for LaserJet 1020', 30)
+model_1.add_to(part_2)
 
-report = Report()
-# создаем объект и добавляем
-part_1 = SparePart('hp', '321')
-report.add_to(part_1)
-part_2 = SparePart('canon', '1211')
-report.add_to(part_2)
+model_2 = Equipment('Cannon 1140')
 
-# выводим журнал
-print(report.dict)
+part_3 = SparePart('Fuser for LaserJet 1020', 50)
+model_2.add_to(part_3)
+part_4 = SparePart('Main Motor for LaserJet 1020', 20)
+model_2.add_to(part_4)
+
+print(model_1.dict)
+print(model_2.dict)
