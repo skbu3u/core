83acb7b076954617721c36c96058fa6e32c66ac3
Divider <89965004+it-cio@users.noreply.github.com>
2022-03-06 20:14:55 +0300

    Add Report tests

--- tests/test_report.py
+++ tests/test_report.py
@@ -6,3 +6,9 @@ def test_init_report():
     model = Equipment('HP LaserJet 1020')
     report = Report(model)
     assert isinstance(report.info, dict)
+
+
+def test_exception_if_report_info_not_a_equipment():
+    with pytest.raises(Exception) as ex:
+        Report([])
+    assert ex.type == ClassInitializationError
