e3436a29251906bf44dda68cbdb19430b4fd551c
Divider <89965004+it-cio@users.noreply.github.com>
2022-03-15 22:32:34 +0300

    getcwd

--- database/sql.py
+++ database/sql.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.getcwd()}/database/sqlite.db"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.getcwd()}/sqlite.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # , echo=True
