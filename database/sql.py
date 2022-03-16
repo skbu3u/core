abbe8733274d06f298f0f228cff768faef240a76
Divider <it-cio@mail.ru>
2022-03-16 13:53:28 +0300

    Rework sql module

--- database/sql.py
+++ database/sql.py
@@ -1,8 +1,14 @@
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pathlib import Path
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///../database/sqlite.db"
+is_database = sorted(Path(sys.path[1]).glob('**/sqlite.db'))
+if is_database:
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///{sys.path[1]}/database/sqlite.db"
+else:
+    SQLALCHEMY_DATABASE_URL = f"sqlite:///database/sqlite.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # , echo=True
@@ -10,3 +16,4 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+connect = engine.connect()
