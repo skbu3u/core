c06d114652576e6e3e873370d39ba6823b2a504f
Divider <it-cio@mail.ru>
2022-03-22 15:11:21 +0300

    Fix bash script

--- script.sh
+++ script.sh
@@ -2,34 +2,22 @@
 # sudo chmod +x script.sh
 clear
 
-echo "try to update"
-git remote update
-count=$(git rev-list HEAD...origin/fast-api --count)
-if [ "$count" -gt "0" ]
-then
-  echo -e "\e[31m"
-  read -r -p  "Update [y/n] " answer
-  echo -e "\e[39m"
-  if [ "$answer" = "y" ]
-  then
-    git fetch --all
-    git reset --hard origin/master
-  fi
-fi
+echo "update core"
+git pull
 
 echo "check database"
 FILE=/src/database/sqlite.db
 if test -f "$FILE"; then
   echo "$FILE exist"
 else
-  echo python3 -m alembic revision --autogenerate 'Initial'
-  echo python3 -m alembic upgrade head
+  python3 -m alembic revision --autogenerate 'Initial'
+  python3 -m alembic upgrade head
 fi
 
 echo "check dependencies"
-echo python3 -m poetry install
+python3 -m poetry install
 
 echo "run server"
 # shellcheck disable=SC2164
 cd src
-echo pytho3 main.py
+pytho3 main.py
