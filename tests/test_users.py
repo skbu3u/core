e464854e9010133257627a0d25e9f8cb4dfaa0d1
Divider <89965004+it-cio@users.noreply.github.com>
2022-04-04 07:29:12 +0300

    Add test for users

---
+++ tests/test_users.py
@@ -0,0 +1,23 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_create_user():
+    response = client.post("/security/register", json={
+        "id": 1,
+        "name": "test_user",
+        "email": "test@mail.com",
+        "password": "test_password"}
+    )
+    assert response.status_code == 201
+    assert response.json() == {'msg': 'User test_user created'}
+
+
+def test_delete_user():
+    response = client.delete("/users")
+    assert response.status_code == 200
+    assert response.json() == []
+    
