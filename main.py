760272f68b5241dddf42ddc93683be4434df439d
Divider <it-cio@mail.ru>
2022-04-13 09:05:46 +0300

    Try to tune middleware

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
