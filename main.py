a4ea33bd08ee62f0803d2616177cf6dca8f30069
Divider <it-cio@mail.ru>
2022-04-13 11:46:21 +0300

    Try to tune middleware

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.gzip import GZipMiddleware
 
 from src.api import security
 from src.api.routes import users, equipments, parts, consumables
@@ -17,11 +16,18 @@ except Exception as ex:
     print(f"Can't load dotenv: {ex}")
     host = '127.0.0.1'
     port = 8000
-# finally:
-#     origins = [f"http://{host}", f"http://{host}:{port}"]
+finally:
+    origins = ["*"]  # [f"http://{host}", f"http://{host}:{port}"]
 
 routes = APIRouter()
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"])
 
 
 @app.get("/")
@@ -36,15 +42,7 @@ routes.include_router(parts)
 routes.include_router(consumables)
 
 app.include_router(routes)
-origins = ["*"]
-app.add_middleware(GZipMiddleware)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"])
+
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host=host, port=port, reload=True, log_level="info")
